282f90c609731e73eb3634ed8c5fa252d6874568
SHULK1033 <andres10333@hotmail.com>
2020-04-08 13:10:41 -0500

    aplicacion de peliculas lista

--- lib/main.dart
+++ lib/main.dart
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Peliculas',
+      title: 'Películas',
       initialRoute: '/',
       routes: {
         '/': (BuildContext context) => HomePage(),
